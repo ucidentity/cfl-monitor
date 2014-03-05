a228ca4f1a6ba6a2b524840a5c40b32cd60f1ba8
Matt Glazier <matthewglazier@gmail.com>
2014-03-05 11:13:59 -0700

    Add sample action script

---
+++ EmailAction.groovy
@@ -0,0 +1,36 @@
+import javax.mail.*
+import javax.mail.internet.*
+import grails.converters.JSON
+import org.codehaus.groovy.grails.web.json.*
+import edu.berkeley.calnet.cflmonitor.domain.History
+import edu.berkeley.calnet.cflmonitor.service.ActionService
+
+name = "Email Action"
+key = "emailAction"
+
+performAction = { config, subject, args ->
+	def props = new Properties()
+	props.put( "mail.smtps.auth", true)
+
+	def session = Session.getDefaultInstance( props, null)
+	def msg = new MimeMessage( session)
+
+	JSONObject jsonArgs = JSON.parse( args)
+
+	msg.setSubject( jsonArgs.subject)
+	msg.setText( jsonArgs.message)
+	msg.addRecipient( Message.RecipientType.TO, new InternetAddress( jsonArgs.recipient))
+
+	def transport = session.getTransport( "smtps")
+
+	try {
+		transport.connect( jsonArgs.host, jsonArgs.username, jsonArgs.password)
+		transport.sendMessage( msg, msg.getAllRecipients())
+
+		ActionService.createHistoryRecord( subject, config.key, "Email sent")
+	}
+	catch( Exception e) {
+	   println "Error in mail"
+	   e.printStackTrace()
+	}
+}
